9775516e67a424e4ed742d9b23524235f2505cb0
Kendriick <kenjclee03@gmail.com>
2026-03-28 23:32:47 +1100

    UPDATE stripe
    - sends the current order into DB table pending orders via server action insert

--- src/lib/actions/stripe.ts
+++ src/lib/actions/stripe.ts
@@ -2,6 +2,8 @@
 
 import { stripe } from '@/lib/stripe/server';
 import { stripeConfig } from '@/lib/stripe/config';
+import { ShippingAddress } from '../types';
+import { createAdminClient } from '../supabase/admin';
 
 // Creates a Stripe PaymentIntent on the server and returns the clientSecret
 // to the browser so it can render the payment form and confirm the payment.
@@ -10,7 +12,26 @@ import { stripeConfig } from '@/lib/stripe/config';
 // Stripe requires amounts in cents (e.g. 15000) - you'll need to convert.
 // OFFICIAL STRIPE DOCS: https://docs.stripe.com/api/payment_intents/create
 
-export const createPaymentIntent = async (amount: number) => {
+// new shapes for storing pending orders
+type PendingCartItem = {
+  product_id: string
+  product_name: string
+  product_image: string | null
+  price: number
+  quantity: number
+}
+
+type PaymentIntentData = {
+  user_id: string,
+  cart_items: PendingCartItem[],
+  shipping_address: ShippingAddress,
+  subtotal: number,
+  tax: number,
+  shipping: number,
+  total: number,
+}
+
+export const createPaymentIntent = async (amount: number, paymentIntentData: PaymentIntentData) => {
   try {
     // Call stripe.paymentIntents.create() with:
     //   - amount: converted to cents (multiply by 100, round to avoid floating point issues)
@@ -18,12 +39,34 @@ export const createPaymentIntent = async (amount: number) => {
     //   - automatic_payment_methods: { enabled: true }
     //     (this lets Stripe show the right payment options for the customer's region)
     //
-    // const paymentIntent = await stripe.paymentIntents.create({ ... })
     const paymentIntent = await stripe.paymentIntents.create({
       amount: Math.round(amount * 100),
       currency: stripeConfig.currency,
       automatic_payment_methods: { enabled: true }
     });
+
+    // we use data recieved from address form to create our pending orders data 
+    const pendingOrder = {
+      user_id: paymentIntentData.user_id, // customer's ID
+      payment_intent_id: paymentIntent.id,
+      cart_items: paymentIntentData.cart_items,
+      shipping_address: paymentIntentData.shipping_address,
+      subtotal: paymentIntentData.subtotal,
+      tax: paymentIntentData.tax,
+      shipping: paymentIntentData.shipping,
+      total: paymentIntentData.total,
+    }
+
+    const supabase = createAdminClient();
+
+    const { error } = await supabase
+      .from("pending_orders")
+      .insert(pendingOrder);
+
+    if (error) {
+      console.error(`Failed to insert pending order: ${error.message}`)
+      throw error;
+    }
 
     // Return an object with two fields:
     //   - clientSecret: paymentIntent.client_secret
@@ -34,7 +77,7 @@ export const createPaymentIntent = async (amount: number) => {
     }
 
   } catch (error) {
-    console.error('Failed to create payment intent:', error)
-    throw new Error('Failed to create payment intent')
+    console.error('Failed to create payment intent:', error);
+    throw new Error('Failed to create payment intent');
   }
 }
